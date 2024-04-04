da7e6edeb7faa588d0054d2c25b77f501e519a99
🦀 adia-dev <abdoudu78130@gmail.com>
2024-04-04 22:26:13 +0200

    Add axiosInstance and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 /// <reference types="@workadventure/iframe-api-typings" />
 
 import { bootstrapExtra } from "@workadventure/scripting-api-extra";
+import axiosInstance from "./axiosInstance";
 
 console.log('Script started successfully');
 
+const jiraBrowseUrl = 'https://jira-mon.atlassian.net/browse/';
+const jiraAssigneeUrl = 'https://jira-mon.atlassian.net/jira/software/projects/JI/boards/1?assignee=';
+const maxInventorySize = 3;
+const maxInventoryMoveDelay = 5000;
 let currentPopup: any = undefined;
 
 // Waiting for the API to be ready
@@ -11,28 +16,31 @@ WA.onInit().then(async () => {
     console.log('Scripting API ready');
     console.log('Player tags: ', WA.player.tags)
 
-    const tickets = []
+    const userIdMap: { [key: string]: string } = {
+        'WOKA_NAME': 'JIRA_ACCOUNT_ID'
+    };
 
-    tickets.forEach((ticket,index)=>{
-        const randomX = Math.floor(Math.random() * 20);
-        const randomY = Math.floor(Math.random() * 20);
-        WA.room.setTiles([
-            {
-                x: randomX,
-                y: randomY,
-                tile: "greenTicket",
-                layer: "furniture/furniture3"
+
+    const jiraIssues = (await axiosInstance.get('jira/issues')).data;
+    const size = initInventory(userIdMap[WA.player.name], jiraIssues);
+    spawnIssues(jiraIssues);
+
+    if (size >= maxInventorySize) {
+        WA.controls.disablePlayerControls();
+
+        const triggerMessage = WA.ui.displayActionMessage({
+            message: "Vous avez trop de tickets, vous ne pouvez plus vous déplacer pendant quelques secondes... Profitez-en pour les traiter !(Appuyer espace pour ouvrir Jira)" + WA.player.name,
+            callback: () => {
+                WA.nav.goToPage(jiraAssigneeUrl + userIdMap[WA.player.name]);
             }
-        ])
-        WA.room.setTiles([
-            {
-                x: 8,
-                y: 13,
-                tile: "greenTicket",
-                layer:"furniture/furniture3"
-            }]
-        )
-    })
+        });
+
+        setTimeout(() => {
+            triggerMessage.remove();
+            WA.controls.restorePlayerControls();
+        }, maxInventoryMoveDelay)
+    }
+
 
     WA.room.area.onEnter('clock').subscribe(() => {
         const today = new Date();
@@ -42,34 +50,7 @@ WA.onInit().then(async () => {
 
     WA.room.area.onLeave('clock').subscribe(closePopup)
 
-    WA.room.area.onEnter('ticket1').subscribe(() => {
-        const triggerMessage = WA.ui.displayActionMessage({
-            message: "Appuyer espace pour s'attribuer le ticket",
-            callback: () => {
-                WA.nav.goToPage('https://jira-mon.atlassian.net/browse/JI-1');
-            }
-        });
-        setTimeout(() => {
-            // later
-            triggerMessage.remove();
-        }, 3000)
-        currentPopup = WA.ui.openPopup("ticket-popup", "Loading...", []);
 
-        fetch("http://localhost:3000/jira", {
-            method: "GET"
-        })
-            .then((response) => {
-                return response.text();
-            })
-            .then((text) => {
-                const jiraTicket = JSON.parse(text);
-                closePopup();
-                currentPopup = WA.ui.openPopup("ticket-popup", jiraTicket.issueTypes[0].description, []);
-            })
-            .catch((err) => console.error(err));
-
-    })
-    WA.room.area.onLeave('ticket1').subscribe(closePopup)
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -83,6 +64,67 @@ function closePopup() {
         currentPopup.close();
         currentPopup = undefined;
     }
+}
+
+function initInventory(userId: string, jiraIssues: any): number {
+    let inventorySize = 0;
+
+    jiraIssues
+        .filter(issue => issue.fields.assignee?.accountId === userId && issue.fields.status.name !== 'DONE' && issue.fields.status.name !== 'BACKLOG')
+        .forEach((issue, index) => {
+            inventorySize++;
+            WA.ui.actionBar.addButton({
+                id: issue.id,
+                toolTip: issue.fields.summary,
+                type: 'action',
+                imageSrc: 'https://pics.clipartpng.com/midle/Red_Dice_PNG_Clip_Art-2654.png',
+                callback: (event) => {
+                    console.log('Inventory item clicked', event);
+                    // When a user clicks on the action bar button 'Register', we remove it.
+                    WA.ui.actionBar.removeButton(issue.id);
+                }
+            });
+        })
+
+    return inventorySize;
+}
+
+function spawnIssues(jiraIssues: any[]) {
+    jiraIssues.forEach((ticket, index) => {
+        const randomX = Math.floor(Math.random() * 20);
+        const randomY = Math.floor(Math.random() * 20);
+        WA.room.setTiles([
+            {
+                x: randomX,
+                y: randomY,
+                tile: "greenTicket",
+                layer: "furniture/furniture3"
+            }
+        ])
+        WA.room.area.create({
+            height: 32,
+            width: 32,
+            x: randomX * 32,
+            y: randomY * 32,
+            name: "ticket" + index,
+        })
+
+        WA.room.area.onEnter('ticket' + index).subscribe(() => {
+            const triggerMessage = WA.ui.displayActionMessage({
+                message: "Appuyer espace pour s'attribuer le ticket",
+                callback: () => {
+                    WA.nav.goToPage(jiraBrowseUrl + ticket.key);
+                }
+            });
+            setTimeout(() => {
+                triggerMessage.remove();
+            }, 3000)
+            currentPopup = WA.ui.openPopup("ticket-popup", ticket.fields.description, []);
+        })
+
+        WA.room.area.onLeave('ticket' + index).subscribe(closePopup)
+    })
+
 }
 
 export { };
